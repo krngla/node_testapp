cf449060cbb26a1045ab44e9a93d8cf8d3eb84d9
Are Nikolai Jevne <Are.Jevne@kitron.com>
2022-12-05 17:04:52 +0100

    a lot lol

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const { app, BrowserWindow } = require('electron')
 
+const {WebSocketServer} = require('ws');
+
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        minWidth: 800,
+
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
     })
 
     win.loadFile('index.html')
@@ -20,3 +28,13 @@ app.whenReady().then(() => {
 app.on('window-all-closed', ()=> {
     if (process.platform !== 'darwin') app.quit()
 })
+
+const wss = new WebSocketServer({port: 8080});
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function message(data) {
+        console.log('recieved: %s', data);
+    });
+
+    ws.send('something');
+});
